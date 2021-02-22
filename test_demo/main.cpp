f75993aea42982d2e5ff227699dd433403428d37
heping.wk <southwindod@foxmail.com>
2021-02-22 13:21:24 +0800

    添加示例代码

--- test_demo/main.cpp
+++ test_demo/main.cpp
@@ -7,6 +7,14 @@
 
 int main()
 {
+	long* pLong1 = new long;
+	char* pChar1 = new char[20];
+	char* pChar2 = (char*)malloc(10);
+
+//	delete pLong1;
+//	delete[] pChar1;
+//	free(pChar2);
+
 	system("pause");
-    return 0;
+	return 0;
 }
